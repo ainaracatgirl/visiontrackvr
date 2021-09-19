21c4c1a59587d5a9ffccbaa024b2303a1af68a71
JanCraft <jancraft888@gmail.com>
2021-09-19 14:11:00 +0200

    Added Direction Tracking

---
+++ README.md
@@ -0,0 +1,15 @@
+# VisionTrackVR
+Tracking your full body anywhere, with just a webcam.
+
+## How it works?
+VisionTrackVR uses a MediaPipe machine-learning model to track the position of your joints and transforms them into usable SteamVR Trackers.
+
+## Quality
+While the quality may not be good, it still counts as full body tracking, and it's practically free.
+
+The quality depends on the power of your computer and your webcam.
+
+## Setup
+
+# License
+MIT License
--- visiontrackvr.py
+++ visiontrackvr.py
@@ -18,6 +18,7 @@ import socket
 import time
 import struct
 import cfgloader
+import math
 
 print("[2/4] Loading configuration...")
 
@@ -61,6 +62,64 @@ def mody(y):
 def modz(z):
     return z * cfg.float("depth_keypoint_scale")
 
+def vecsub(a, b):
+    return {'x':a['x']-b['x'],'y':a['y']-b['y'],'z':a['z']-b['z']}
+
+def veccross(a, b):
+    return {'x':a['y'] * b['z'] - a['z'] * b['y'],'y':a['z'] * b['x'] - a['x'] * b['z'],'z':a['x'] * b['y'] - a['y'] * b['x']}
+
+def vecdot(a, b):
+    return a['x']*b['x'] + a['y']*b['y'] + a['z']*b['z']
+
+def vecsqrmag(v):
+    return v['x'] * v['x'] + v['y'] * v['y'] + v['z'] * v['z']
+
+def clamp(x, _min, _max):
+    return min(max(x, _min), _max)
+
+def vecangle(a, b):
+    denominator = math.sqrt(vecsqrmag(a) * vecsqrmag(b))
+    if denominator < (10.0 ** -15): return 0.0
+
+    dot = clamp(vecdot(a, b) / denominator, -1.0, 1.0)
+    return math.degrees(math.acos(dot))
+
+def vecnormalize(v):
+    l = math.sqrt(vecsqrmag(v))
+    return {'x':v['x']/l,'y':v['y']/l,'z':v['z']/l}
+
+def vecscale(v, s):
+    return {'x':v['x']*s,'y':v['y']*s,'z':v['z']*s}
+
+def quatvector(x, y, z, w):
+    t0 = +2.0 * (w * x + y * z)
+    t1 = +1.0 - 2.0 * (x * x + y * y)
+    roll_x = math.atan2(t0, t1)
+    
+    t2 = +2.0 * (w * y - z * x)
+    t2 = +1.0 if t2 > +1.0 else t2
+    t2 = -1.0 if t2 < -1.0 else t2
+    pitch_y = math.asin(t2)
+    
+    t3 = +2.0 * (w * z + x * y)
+    t4 = +1.0 - 2.0 * (y * y + z * z)
+    yaw_z = math.atan2(t3, t4)
+    
+    return {'x':roll_x,'y':pitch_y,'z':yaw_z}
+
+def quatangleaxis(angle, axis):
+    rad = math.radians(angle * 0.5)
+    axis2 = vecscale(axis, math.sin(rad))
+    return quatvector(axis2['x'], axis2['y'], axis2['z'], math.cos(rad))
+
+def fromtorotation(aFrom, aTo):
+    axis = veccross(aFrom, aTo)
+    angle = vecangle(aFrom, aTo)
+    return quatangleaxis(angle, vecnormalize(axis))
+
+def angle_from_points(a, b):
+    return fromtorotation({'x':0,'y':1,'z':0}, vecsub(b, a))
+
 keypoints = []
 def get_keypoints(pose_landmarks):
     keypoints.clear()
@@ -85,6 +144,7 @@ cv2.namedWindow("VisionTrackVR")
 
 with mp_pose.Pose(
         smooth_landmarks=True,
+        model_complexity=cfg.int('model_complexity'),
         min_detection_confidence=0.5,
         min_tracking_confidence=0.5) as pose:
     while cap.isOpened():
@@ -123,10 +183,13 @@ with mp_pose.Pose(
             TRACKER_RIGHT_FOOT[1] = mody(1 - right_ankle['y'])
             TRACKER_RIGHT_FOOT[2] = modz(right_ankle['z'])
 
+            hiprot = math.degrees(math.atan2(left_hip['x'] - right_hip['x'], right_hip['z'] - left_hip['z']))
+
             if lastkey == ord('c'):
                 calib.values['hips_x'] = -TRACKER_HIPS[0] + 0
                 calib.values['hips_y'] = -TRACKER_HIPS[1] + 0.5
                 calib.values['hips_z'] = -TRACKER_HIPS[2] + 0
+                calib.values['hips_r'] = -hiprot
 
                 calib.values['left_foot_x'] = -TRACKER_LEFT_FOOT[0] + 0.25
                 calib.values['left_foot_y'] = -TRACKER_LEFT_FOOT[1] + -1
@@ -137,13 +200,18 @@ with mp_pose.Pose(
                 calib.values['right_foot_z'] = -TRACKER_RIGHT_FOOT[2] + 0
 
                 cfgloader.dump(open('calibration.cfg', 'w'), calib, "Auto-generated calibration values")
+            
+            if lastkey == ord('r'):
+                cfg = cfgloader.load(open("config.cfg", 'r'))
+                calib = cfgloader.load(open("calibration.cfg", 'r'))
 
             if hips['visibility'] > CONFIG_TRACKER_CONFIDENCE: tracker_count += 1
             if left_ankle['visibility'] > CONFIG_TRACKER_CONFIDENCE: tracker_count += 1
             if right_ankle['visibility'] > CONFIG_TRACKER_CONFIDENCE: tracker_count += 1
 
-            sock.sendto(struct.pack("ddddddddd", 
+            sock.sendto(struct.pack("dddddddddd", 
                 TRACKER_HIPS[0] + calib.float("hips_x"), TRACKER_HIPS[1] + calib.float("hips_y"), TRACKER_HIPS[2] + calib.float("hips_z"),
+                hiprot + 270 + calib.float("hips_r"),
                 TRACKER_LEFT_FOOT[0] + calib.float("left_foot_x"), TRACKER_LEFT_FOOT[1] + calib.float("left_foot_y"), TRACKER_LEFT_FOOT[2] + calib.float("left_foot_z"),
                 TRACKER_RIGHT_FOOT[0] + calib.float("right_foot_x"), TRACKER_RIGHT_FOOT[1] + calib.float("right_foot_y"), TRACKER_RIGHT_FOOT[2] + calib.float("right_foot_z")
             ), (CONFIG_UDP_IP, CONFIG_UDP_PORT))
